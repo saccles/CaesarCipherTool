f0f450a856de73fc3b890f4c6e65869de9a3e5ae
wraith <formaljek14@gmail.com>
2024-05-14 20:42:45 +0000

    Delete COM_conclusion.py

--- COM_conclusion.py
+++
@@ -1,8 +0,0 @@
-from caesar_cipher_functions import encrypt, decrypt
-from basic_animation_functions import animate, sleep
-
-secret_msg = encrypt("Thanks for listening. Bye for now!")
-animate(decrypt(f"\n{secret_msg}"))
-print()
-
-
