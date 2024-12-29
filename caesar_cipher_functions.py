632e0de2350efe6a1375960ae125d434e04efaf6
saccles <silasaccles@gmail.com>
2024-12-28 20:12:32 -0500

    Reformat caesar_cipher_functions.py and modify docstrings

--- caesar_cipher_functions.py
+++ caesar_cipher_functions.py
@@ -2,13 +2,26 @@ from string import ascii_lowercase
 
 VALID_CHARS = [letter for letter in ascii_lowercase]
 
+
 def encrypt(plaintext, key=3):
-    """A function that encrypts a string using the Caesar cipher."""
+    """
+    Encrypt a plaintext string using the Caesar cipher 
+    and return a ciphertext string.
+
+    Arguments:
+        plaintext (str): the message or text to encrypt
+        key (int, keyword): the encryption key (default 3)
+
+    Returns:
+        ciphertext (str): the ciphertext corresponding to the plaintext
+    """
     ciphertext = ""
     for char in plaintext:
         if char.lower() in VALID_CHARS:
+            
             # Get position of character.
             index = VALID_CHARS.index(char.lower())
+            
             # Perform encryption operations.
             new_index = (index + key) % 26
             new_char = VALID_CHARS[new_index]
@@ -19,13 +32,26 @@ def encrypt(plaintext, key=3):
         ciphertext += new_char
     return ciphertext
 
+
 def decrypt(ciphertext, key=3):
-    """A function that decrypts a string using the Caesar cipher."""
+    """
+    Decrypt a ciphertext string using the Caesar cipher 
+    and return a plaintext string.
+
+    Arguments:
+        ciphertext (str): the message or text to decrypt
+        key (int, keyword): the decryption key (default 3)
+
+    Returns:
+        plaintext (str): the plaintext corresponding to the ciphertext
+    """
     plaintext = ""
     for char in ciphertext:
         if char.lower() in VALID_CHARS:
+            
             # Get position of character.
             index = VALID_CHARS.index(char.lower())
+            
             # Perform decryption operations. 
             new_index = (index - key) % 26
             new_char = VALID_CHARS[new_index]
