308623b12ac29a2103cd417fbd09e21ff22258f6
saccles <silasaccles@gmail.com>
2025-01-12 22:12:33 -0500

    Update basic_animation_functions.py docstrings.

--- basic_animation_functions.py
+++ basic_animation_functions.py
@@ -1,10 +1,14 @@
 from time import sleep
 
 
-def animate(text, time=0.10, colors=None):
+def animate(text, time=0.10):
     """
     Add basic animation to inputted text in the form of a slight delay
     between the printing of each character in the text.
+    
+    Arguments:
+        text (str): the text to animate
+        time (float, keyword): the delay between animating each character (default 0.10 seconds)
     """
     for char in text:
         print(char, end="", flush=True)
